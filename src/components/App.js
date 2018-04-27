9c92ed4faade446898952c158a9d259d9ea4662d
SherryH <enjoy.wind7rain@gmail.com>
2018-04-27 23:00:49 +1200

    Separate Heading component

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import ContactList from './ContactList';
 import './App.css';
-
-const Heading = () => (
-  <div className="jumbotron jumbotron-fluid">
-    <div className="container">
-      <h1 className="display-4">Contact List </h1>
-      <p className="lead">This is a contact list that offers search and view details capability</p>
-    </div>
-  </div>
-);
+import './Heading';
 
 const NameCard = () => (
-  <div className="card">
-    <div className="card-header">Featured</div>
-    <div className="card-body">
-      <h5 className="card-title">Special title treatment</h5>
-      <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  <div className="cardWrapper">
+    <div className="card">
+      <div className="card-header">Featured</div>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
     </div>
   </div>
 );
---
+++ src/components/Heading.js
@@ -0,0 +1,14 @@
+/* @flow */
+
+import React from 'react';
+
+const Heading = () => (
+  <header className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Contact List </h1>
+      <p className="lead">This is a contact list that offers search and view details capability</p>
+    </div>
+  </header>
+);
+
+export default Heading;
