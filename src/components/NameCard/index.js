c1ddf93d3e4924cabf0cf82f5944d8bf08302c54
SherryH <enjoy.wind7rain@gmail.com>
2018-04-29 16:43:56 +1200

    Created styled namecard with dummy info

--- src/components/NameCard/index.js
+++ src/components/NameCard/index.js
@@ -1,14 +1,29 @@
 /* @flow */
 
 import React from 'react';
+import css from './NameCard.css';
 
 const NameCard = () => (
-  <div className="cardWrapper">
-    <div className="card">
-      <div className="card-header">Featured</div>
-      <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  <div className={css.wrapper}>
+    <div className={css.nameCard}>
+      <div className={css.contactTitle}>
+        <div className={css.name}>John Smith</div>
+        <div className={css.title}>Solution Engineer</div>
+      </div>
+      <div className={css.divider} />
+      <div className={css.contactDetails}>
+        <div className={css.description}>Transition Cutting-Edge Web</div>
+        <div className={css.contactMethods}>
+          <div className={css.phone}>
+            <strong>Phone:</strong> 493.170.9623
+          </div>
+          <div className={css.email}>
+            <strong>Email:</strong> nathan@yesenia.net
+          </div>
+          <div className={css.url}>
+            <strong>URL:</strong> ramiro.info
+          </div>
+        </div>
       </div>
     </div>
   </div>
