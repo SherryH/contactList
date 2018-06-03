44a910569db07d134805fbf9f1f029ae0bf4785c
SherryH <enjoy.wind7rain@gmail.com>
2018-06-04 08:23:14 +1200

    Moved fontawesome, router to main index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,32 +1,21 @@
 /* @flow */
 
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
-import fontawesome from '@fortawesome/fontawesome';
-import faSort from '@fortawesome/fontawesome-free-solid/faSort';
-import faUp from '@fortawesome/fontawesome-free-solid/faLongArrowAltUp';
-import faDown from '@fortawesome/fontawesome-free-solid/faLongArrowAltDown';
 import LoadingBar from 'react-redux-loading-bar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Route } from 'react-router-dom';
 import Heading from '../Heading';
 import NavBar from '../NavBar';
 import Report from '../Report';
 import Home from '../Home';
 
-fontawesome.library.add(faSort, faUp, faDown);
-
 const App = () => (
-  <Router>
-    <div>
-      <LoadingBar loading={1} />
-      <NavBar />
-      <Heading />
-      <Route exact path="/" component={Home} />
-      <Route path="/report" component={Report} />
-    </div>
-  </Router>
+  <React.Fragment>
+    <LoadingBar loading={1} />
+    <NavBar />
+    <Heading />
+    <Route exact path="/" component={Home} />
+    <Route path="/report" component={Report} />
+  </React.Fragment>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import { BrowserRouter as Router } from 'react-router-dom';
+import fontawesome from '@fortawesome/fontawesome';
+import faSort from '@fortawesome/fontawesome-free-solid/faSort';
+import faUp from '@fortawesome/fontawesome-free-solid/faLongArrowAltUp';
+import faDown from '@fortawesome/fontawesome-free-solid/faLongArrowAltDown';
 import App from './components/App/index';
 import configureStore from './configureStore';
+
+fontawesome.library.add(faSort, faUp, faDown);
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
